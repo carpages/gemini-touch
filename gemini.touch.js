b201975744e46cd444c42821f1c653686bee420e
Matt Rose <mrose@carpages.ca>
2014-06-11 09:05:07 -0400

    Added documentation

--- gemini.touch.js
+++ gemini.touch.js
@@ -1,3 +1,22 @@
+/**
+ * @fileoverview
+
+A Gemini module to load on mobile devices.
+
+### Features
+- Loads [hammer.js](http://eightmedia.github.io/hammer.js/)
+- Loads [fastclick](https://github.com/ftlabs/fastclick)
+
+ *
+ * @namespace gemini.touch
+ * @copyright Carpages.ca 2014
+ * @author Matt Rose <matt@mattrose.ca>
+ *
+ * @requires gemini-loader
+ * @requires jquery.hammer
+ * @requires fastclick
+ *
+ */
 define(['gemini-loader', 'jquery.hammer', 'fastclick'], function(GEM){
 
 	//Initiate fast click
