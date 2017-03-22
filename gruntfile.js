a06230824413487f8f79c16a4b29d12cff8bdaab
Jeff Andrews <jeff.andrews@carpages.ca>
2017-03-22 13:49:09 -0400

    Clean up whitespace

--- gruntfile.js
+++ gruntfile.js
@@ -3,13 +3,14 @@
 module.exports = function( grunt ) {
   // Load all grunt tasks
   require( 'load-grunt-tasks' )( grunt );
+
   // Show elapsed time at the end
   require( 'time-grunt' )( grunt );
 
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg:    grunt.file.readJSON( 'package.json' ),
+    pkg: grunt.file.readJSON( 'package.json' ),
     banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
@@ -20,8 +21,8 @@ module.exports = function( grunt ) {
       all: {
         options: {
           timeout: 10000,
-          urls:    [ 'http://localhost:9000/test/<%= pkg.name %>.html' ],
-          page:    {
+          urls: [ 'http://localhost:9000/test/<%= pkg.name %>.html' ],
+          page: {
             viewportSize: { width: 10000, height: 10000 }
           }
         }
@@ -37,95 +38,95 @@ module.exports = function( grunt ) {
       server: {
         options: {
           hostname: '*',
-          port:     9000
+          port: 9000
         }
       }
     },
     'saucelabs-qunit': {
       all: {
         options: {
-          urls:     [ 'http://localhost:9000/test/<%= pkg.name %>.html' ],
-          build:    process.env.TRAVIS_JOB_ID,
+          urls: [ 'http://localhost:9000/test/<%= pkg.name %>.html' ],
+          build: process.env.TRAVIS_JOB_ID,
           browsers: [
             // iOS
             {
               browserName: 'iphone',
-              platform:    'OS X 10.9',
-              version:     '7.1'
+              platform: 'OS X 10.9',
+              version: '7.1'
             },
             {
               browserName: 'ipad',
-              platform:    'OS X 10.9',
-              version:     '7.1'
+              platform: 'OS X 10.9',
+              version: '7.1'
             },
             // Android
             {
               browserName: 'android',
-              platform:    'Linux',
-              version:     '4.3'
+              platform: 'Linux',
+              version: '4.3'
             },
             // OS X
             {
               browserName: 'safari',
-              platform:    'OS X 10.9',
-              version:     '7'
+              platform: 'OS X 10.9',
+              version: '7'
             },
             {
               browserName: 'safari',
-              platform:    'OS X 10.8',
-              version:     '6'
+              platform: 'OS X 10.8',
+              version: '6'
             },
             {
               browserName: 'firefox',
-              platform:    'OS X 10.9',
-              version:     '28'
+              platform: 'OS X 10.9',
+              version: '28'
             },
             // Windows
             {
               browserName: 'internet explorer',
-              platform:    'Windows 8.1',
-              version:     '11'
+              platform: 'Windows 8.1',
+              version: '11'
             },
             {
               browserName: 'internet explorer',
-              platform:    'Windows 8',
-              version:     '10'
+              platform: 'Windows 8',
+              version: '10'
             },
             {
               browserName: 'internet explorer',
-              platform:    'Windows 7',
-              version:     '11'
+              platform: 'Windows 7',
+              version: '11'
             },
             {
               browserName: 'internet explorer',
-              platform:    'Windows 7',
-              version:     '10'
+              platform: 'Windows 7',
+              version: '10'
             },
             {
               browserName: 'internet explorer',
-              platform:    'Windows 7',
-              version:     '9'
+              platform: 'Windows 7',
+              version: '9'
             },
             {
               browserName: 'internet explorer',
-              platform:    'Windows 7',
-              version:     '8'
+              platform: 'Windows 7',
+              version: '8'
             },
             {
               browserName: 'firefox',
-              platform:    'Windows 7',
-              version:     '29'
+              platform: 'Windows 7',
+              version: '29'
             },
             {
               browserName: 'chrome',
-              platform:    'Windows 7',
-              version:     '34'
+              platform: 'Windows 7',
+              version: '34'
             },
             // Linux
             {
               browserName: 'firefox',
-              platform:    'Linux',
-              version:     '29'
+              platform: 'Linux',
+              version: '29'
             }
           ]
         }
